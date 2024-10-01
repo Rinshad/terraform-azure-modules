76046614dee0bed19b682ad74721f5e997c8707d
Rinshad rabab <n286535@pagonxt.com>
2024-10-01 17:32:11 +0200

    output modified

--- vnet/outputs.tf
+++ vnet/outputs.tf
@@ -2,3 +2,10 @@ output "vnet_id" {
   description = "The ID of the Virtual Network"
   value       = azurerm_virtual_network.vnet.id
 }
+output "resource_group_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
