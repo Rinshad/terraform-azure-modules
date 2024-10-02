7be2c99a76759363938669067ea70e0644e1c5d7
Rinshad <rinshadrabab@gmail.com>
2024-10-02 18:28:51 +0200

    Create variables.tf

---
+++ kubernetes/variables.tf
@@ -0,0 +1,206 @@
+variable "location" {
+  description = "Azure location of the resources."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group to be imported."
+  type        = string
+}
+
+variable "name" {
+  description = "Name of the subnet to create."
+  type        = string
+}
+
+variable "azure_policy_enabled" {
+  type        = bool
+  description = "Enable Azure Policy Addon."
+  default     = true
+}
+
+variable "disk_encryption_set_id" {
+  type        = string
+  description = "(Optional) The ID of the Disk Encryption Set which should be used for the Nodes and Volumes. More information [can be found in the documentation](https://docs.microsoft.com/azure/aks/azure-disk-customer-managed-keys). Changing this forces a new resource to be created."
+  default     = null
+}
+
+variable "http_application_routing_enabled" {
+  type        = bool
+  description = "Enable HTTP Application Routing Addon (forces recreation)."
+  default     = false
+}
+
+variable "kubernetes_version" {
+  type        = string
+  description = "Specify which Kubernetes release to use. The default used is the latest Kubernetes version available in the region"
+  default     = "1.25.6"
+}
+
+variable "local_account_disabled" {
+  type        = bool
+  description = "(Optional) - If `true` local accounts will be disabled. Defaults to `false`. See [the documentation](https://docs.microsoft.com/azure/aks/managed-aad#disable-local-accounts) for more information."
+  default     = null
+}
+
+variable "node_resource_group" {
+  type        = string
+  description = "The auto-generated Resource Group which contains the resources for this Managed Kubernetes Cluster. Changing this forces a new resource to be created."
+  default     = null
+}
+
+variable "oidc_issuer_enabled" {
+  description = "Enable or Disable the OIDC issuer URL. Defaults to false."
+  type        = bool
+  default     = false
+}
+
+variable "private_cluster_enabled" {
+  type        = bool
+  description = "If true cluster API server will be exposed only on internal IP address and available only in"
+  default     = true
+  validation {
+    condition     = can(regex("^(true)$", var.private_cluster_enabled))
+    error_message = "The cluster should be always private."
+  }
+}
+
+variable "private_cluster_public_fqdn_enabled" {
+  type        = bool
+  description = "(Optional) Specifies whether a Public FQDN for this Private Cluster should be added. Defaults to `false`."
+  default     = false
+}
+
+variable "private_dns_zone_id" {
+  type        = string
+  description = "(Optional) Either the ID of Private DNS Zone which should be delegated to this Cluster, `System` to have AKS manage this or `None`. In case of `None` you will need to bring your own DNS server and set up resolving, otherwise cluster will have issues after provisioning. Changing this forces a new resource to be created."
+  default     = null
+}
+
+variable "role_based_access_control_enabled" {
+  type        = bool
+  description = "Enable Role Based Access Control."
+  default     = false
+  nullable    = false
+}
+
+variable "sku_tier" {
+  type        = string
+  description = "The SKU Tier that should be used for this Kubernetes Cluster. Possible values are Free and Paid"
+  default     = "Free"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Any tags that should be present on the AKS cluster resources"
+  default     = {}
+}
+
+variable "agents_pool_name" {
+  type        = string
+  description = "The default Azure AKS agentpool (nodepool) name."
+  default     = "defaultpool"
+  nullable    = false
+}
+
+variable "agents_size" {
+  type        = string
+  description = "The default virtual machine size for the Kubernetes agents"
+  default     = "Standard_D2s_v3"
+}
+
+variable "enable_host_encryption" {
+  type        = bool
+  description = "Enable Host Encryption for default node pool. Encryption at host feature must be enabled on the subscription: https://docs.microsoft.com/azure/virtual-machines/linux/disks-enable-host-based-encryption-cli"
+  default     = false
+}
+
+variable "temporary_name_for_rotation" {
+  type        = string
+  description = "Specifies the name of the temporary node pool used to cycle the default node pool for VM resizing."
+  default     = "tempopool"
+}
+
+variable "enable_node_public_ip" {
+  type        = bool
+  description = "(Optional) Should nodes in this Node Pool have a Public IP Address? Defaults to false."
+  default     = false
+}
+
+variable "enable_auto_scaling" {
+  type        = bool
+  description = "Enable node pool autoscaling"
+  default     = false
+}
+
+variable "agents_max_pods" {
+  type        = number
+  description = "(Optional) The maximum number of pods that can run on each agent. Changing this forces a new resource to be created."
+  default     = null
+}
+
+variable "agents_count" {
+  type        = number
+  description = "The number of Agents that should exist in the Agent Pool. Please set `agents_count` `null` while `enable_auto_scaling` is `true` to avoid possible `agents_count` changes."
+  default     = 2
+}
+
+variable "agents_labels" {
+  type        = map(string)
+  description = "(Optional) A map of Kubernetes labels which should be applied to nodes in the Default Node Pool. Changing this forces a new resource to be created."
+  default     = {}
+}
+
+variable "only_critical_addons_enabled" {
+  type        = bool
+  description = "(Optional) Enabling this option will taint default node pool with `CriticalAddonsOnly=true:NoSchedule` taint. Changing this forces a new resource to be created."
+  default     = null
+}
+
+variable "orchestrator_version" {
+  type        = string
+  description = "Specify which Kubernetes release to use for the orchestration layer. The default used is the latest Kubernetes version available in the region"
+  default     = null
+}
+
+variable "os_disk_size_gb" {
+  type        = number
+  description = "Disk size of nodes in GBs."
+  default     = null
+}
+
+variable "os_disk_type" {
+  type        = string
+  description = "The type of disk which should be used for the Operating System. Possible values are `Ephemeral` and `Managed`. Defaults to `Managed`. Changing this forces a new resource to be created."
+  default     = null
+}
+
+variable "agents_tags" {
+  type        = map(string)
+  description = "(Optional) A mapping of tags to assign to the Node Pool."
+  default     = {}
+}
+
+variable "agents_type" {
+  type        = string
+  description = "(Optional) The type of Node Pool which should be created. Possible values are AvailabilitySet and VirtualMachineScaleSets. Defaults to VirtualMachineScaleSets."
+  default     = "VirtualMachineScaleSets"
+}
+
+variable "ultra_ssd_enabled" {
+  type        = bool
+  description = "(Optional) Used to specify whether the UltraSSD is enabled in the Default Node Pool. Defaults to false."
+  default     = false
+}
+
+variable "vnet_subnet_id" {
+  type        = string
+  description = "(Optional) The ID of a Subnet where the Kubernetes Node Pool should exist. Changing this forces a new resource to be created."
+  default     = null
+}
+
+variable "agents_availability_zones" {
+  type        = list(string)
+  description = "(Optional) A list of Availability Zones across which the Node Pool should be spread. Changing this forces a new resource to be created."
+  default     = null
+}
