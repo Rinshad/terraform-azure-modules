fe8af08593237161986b5a6e0e41152502cc0e9c
Rinshad <rinshadrabab@gmail.com>
2024-10-03 16:24:39 +0200

    Update outputs.tf

--- vnet/outputs.tf
+++ vnet/outputs.tf
@@ -3,5 +3,5 @@ output "vnet_id" {
   value       = azurerm_virtual_network.vnet.id
 }
 output "resource_group_name" {
-  value = var.resource_group_name
+  value = azurerm_resource_group.vnet_rg.name
 }
