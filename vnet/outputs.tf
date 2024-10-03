e442f5041d8f7520ec9ce4f33a04036b1ffa3a51
Rinshad <rinshadrabab@gmail.com>
2024-10-03 16:22:36 +0200

    Update outputs.tf

--- vnet/outputs.tf
+++ vnet/outputs.tf
@@ -2,3 +2,6 @@ output "vnet_id" {
   description = "The ID of the Virtual Network"
   value       = azurerm_virtual_network.vnet.id
 }
+output "resource_group_name" {
+  value = var.resource_group_name
+}
