9cf357a7b659286a26df054cbd6ec59b1dc1da38
Rinshad <rinshadrabab@gmail.com>
2024-10-04 15:10:07 +0200

    Update outputs.tf

--- kubernetes/outputs.tf
+++ kubernetes/outputs.tf
@@ -1,14 +1,9 @@
 output "cluster_name" {
   description = "The k8s cluster name."
-  value       = azurerm_kubernetes_cluster.this.name
+  value       = azurerm_kubernetes_cluster.aks.name
 }
 
 output "cluster_id" {
   description = "The k8s cluster ID."
-  value       = azurerm_kubernetes_cluster.this.id
-}
-
-output "aks_api" {
-  description = "The k8s cluster API hostname."
-  value       = azurerm_private_dns_a_record.aks_endpoint.fqdn
+  value       = azurerm_kubernetes_cluster.aks.id
 }
